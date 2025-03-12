bf556b4cf9e49e9090d55a9cd0fa76a4f7562321
abdarum <kornel.stefanczyk@vtech-pl.com>
2025-03-12 20:36:03 +0100

    Implemented functionality of device
    But it's size is too big

--- Projekty/Sterownik_LED_Symulacja_swiatla_dziennego/Sterownik_LED_Symulacja_swiatla_dziennego.ino
+++ Projekty/Sterownik_LED_Symulacja_swiatla_dziennego/Sterownik_LED_Symulacja_swiatla_dziennego.ino
@@ -13,7 +13,7 @@ There will be one button used to set two parameters. Two types of presses will b
 Modes activated by pressing:
 - Shortpress: Activates the mode for setting the maximum LED brightness. The LED will light up to the maximum available brightness, and the potentiometer can be used to set the max brightness (PWM). The LED brightness will be updated in real-time during adjustment. After entering the max brightness setting mode, there will be 3 minutes to confirm the new brightness by pressing shortpress. If shortpress is not pressed, the device will return to the standard mode without setting the current value.
 
-- Longpress: Activates the mode for setting the minimum value. After entering this mode, the user will have 3 minutes to confirm the current value read from the photoresistor as the minimum value. Confirmation is done by shortpress, and if there is no response, the device will return to standard mode. This mode will be signaled by displaying three LED brightness values sequentially, each for 0.5 seconds:
+- Longpress: Activates the mode for setting the minimum value. After entering this mode, the user will have 3 minutes to confirm the current value read from the photoresistor as the minimum value. Confirmation is done by shortpress, and if there is no response, the device will return to standard mode. This mode will be signaled by displaying three LED brightness values sequentially, each for 1.5 seconds:
   - 0% PWM
   - LED brightness corresponding to the current light level on the photoresistor, and the current minimum threshold
   - max LED brightness (max PWM)
@@ -54,58 +54,200 @@ https://github.com/wagiminator/ATtiny13-TinyDecoder
 
 #define SERIAL_ENA 0
 
-#define LED_PWM_OUT_PIN 0 // Pin PWM dla paska LED
-#define PHOTORESISTOR_PIN A2 // Pin fotorezystora
-#define MAX_PWM_POT_PIN A3 // Potencjometry do ustawiania max PWM
+#define LED_PWM_OUT_PIN 0    // PWM pin for the LED strip
+#define PHOTORESISTOR_PIN A1 // Pin for the photoresistor
+#define MAX_PWM_POT_PIN A3   // Potentiometer for setting max PWM
+#define BUTTON_PIN 1         // Pin for the button
 
-
-#define MIN_VOLTAGE_TRESCHOLD 0.1 // 10%~, ~0.5V , ~650k Ohm 
+#define MIN_VOLTAGE_DEFAULT_THRESHOLD 0.1  // 10%~, ~0.5V , ~650k Ohm
+#define MAX_VOLTAGE_DEFAULT_THRESHOLD 0.85 // 85%~, ~1.5k Ohm
 #define MAX_ADC_VALUE 1024
 #define MAX_PWM_VALUE 255
 
+enum Mode
+{
+  STANDARD,
+  SETTING_MIN_BRIGHTNESS,
+  SETTING_MAX_PWM
+};
+
+enum ButtonPress
+{
+  NO_PRESS,
+  SHORT_PRESS,
+  LONG_PRESS
+};
+
+Mode currentMode = STANDARD;
+ButtonPress buttonPress = NO_PRESS;
+
 INT maxPwm = 0;
-INT photoresAdcValueMin = MIN_VOLTAGE_TRESCHOLD*MAX_ADC_VALUE;
-INT photoresAdcValueMax = MIN_VOLTAGE_TRESCHOLD;
+INT photoresistorAdcValueMin = MIN_VOLTAGE_DEFAULT_THRESHOLD * MAX_ADC_VALUE;
+INT photoresistorAdcValueMax = MAX_VOLTAGE_DEFAULT_THRESHOLD * MAX_ADC_VALUE;
+ULONG lastPressTime = 0;
+ULONG lastButtonReadTime = 0;
+ULONG lastModeChangeTime = 0;
 
-UINT map_uint(UINT x, UINT in_min, UINT in_max, UINT out_min, UINT out_max) {
-    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+#if SERIAL_ENA
+ULONG lastSerialSendTime = 0;
+#endif
+
+UINT map_uint(UINT x, UINT in_min, UINT in_max, UINT out_min, UINT out_max)
+{
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
-void setup() {
-  pinMode(LED_PWM_OUT_PIN, OUTPUT);
-  #if SERIAL_ENA
-  Serial.begin(9600);
-  #endif
+UINT getAdjustmentValuePwmMapped()
+{
+  return analogRead(MAX_PWM_POT_PIN) * MAX_PWM_VALUE / MAX_ADC_VALUE;
 }
 
-void loop() {
-  // Odczyt wartości z potencjometru max wypełnienie PWM + Adaptacja do max dla PWM
-  maxPwm = analogRead(MAX_PWM_POT_PIN) * MAX_PWM_VALUE / MAX_ADC_VALUE; // Odczyt max wypełnienie PWM
+UINT getSensorValue()
+{
+  return analogRead(PHOTORESISTOR_PIN);
+}
 
-  // Odczyt wartości z fotorezystora
-  INT sensorValue = analogRead(PHOTORESISTOR_PIN);
+ButtonPress checkButtonPress()
+{
+  BOOL buttonState = digitalRead(BUTTON_PIN);
+  ULONG currentTime = millis();
 
-  INT pwmValue = 0;
-  if (sensorValue > photoresAdcValueMin)
+  if (buttonState == HIGH)
+  { // Button pressed (pulldown configuration)
+    if (currentTime - lastPressTime > 6000)
+    {
+      return NO_PRESS;
+    }
+    else if (currentTime - lastPressTime > 2000)
+    {
+      return LONG_PRESS;
+    }
+    else if (currentTime - lastPressTime > 500)
+    {
+      return SHORT_PRESS;
+    }
+  }
+  else
   {
-    // Mapowanie wartości fotorezystora na zakres PWM
-    pwmValue = map_uint(sensorValue, photoresAdcValueMin, photoresAdcValueMax, 0, maxPwm);
-    pwmValue = constrain(pwmValue, 0, maxPwm); // Ograniczenie wartości PWM do maxPwm
+    lastPressTime = currentTime;
   }
 
-  analogWrite(LED_PWM_OUT_PIN, pwmValue); // Ustawienie sygnału PWM na pinie LED
+  return NO_PRESS;
+}
 
-  #if SERIAL_ENA
-  // Debugowanie
-  Serial.print("Sensor Value: ");
-  Serial.print(sensorValue);
-  Serial.print(" | PWM Value: ");
-  Serial.println(pwmValue);
-  Serial.print(" | Min Value: ");
-  Serial.println(photoresAdcValueMin);
-  Serial.print(" | Max Value: ");
-  Serial.println(photoresAdcValueMax);
-  #endif
+void updateLedBrightness()
+{
+  ULONG currentTime = millis();
+  INT pwmValue = 0;
+  INT sensorValue = getSensorValue();
+  if (sensorValue > photoresistorAdcValueMin)
+  {
+    pwmValue = map_uint(sensorValue, photoresistorAdcValueMin, photoresistorAdcValueMax, 0, maxPwm);
+    pwmValue = constrain(pwmValue, 0, maxPwm);
+  }
 
-  delay(100); // Małe opóźnienie dla stabilności odczytów
+  switch (currentMode)
+  {
+  case STANDARD:
+  {
+    analogWrite(LED_PWM_OUT_PIN, pwmValue);
+  }
+  break;
+
+  case SETTING_MAX_PWM:
+    INT tmpMaxPwm = getAdjustmentValuePwmMapped();
+    analogWrite(LED_PWM_OUT_PIN, tmpMaxPwm);
+    break;
+
+  case SETTING_MIN_BRIGHTNESS:
+    if (currentTime - lastModeChangeTime < 1500)
+    {
+      analogWrite(LED_PWM_OUT_PIN, 0);
+    }
+    else if (currentTime - lastModeChangeTime < 3000)
+    {
+      analogWrite(LED_PWM_OUT_PIN, pwmValue);
+    }
+    else if (currentTime - lastModeChangeTime < 4500)
+    {
+      analogWrite(LED_PWM_OUT_PIN, maxPwm);
+    }
+    else
+    {
+      lastModeChangeTime = currentTime;
+    }
+    break;
+  }
+
+#if SERIAL_ENA
+  if (currentTime - lastSerialSendTime > 500)
+  {
+    Serial.print("Sensor Value: ");
+    Serial.print(sensorValue);
+    Serial.print(" | PWM Value: ");
+    Serial.println(pwmValue);
+    Serial.print(" | Min Value: ");
+    Serial.println(photoresistorAdcValueMin);
+    Serial.print(" | Max Value: ");
+    Serial.println(photoresistorAdcValueMax);
+    lastSerialSendTime = currentTime;
+  }
+#endif
+}
+
+void setup()
+{
+  pinMode(LED_PWM_OUT_PIN, OUTPUT);
+  pinMode(BUTTON_PIN, INPUT);
+#if SERIAL_ENA
+  Serial.begin(9600);
+#endif
+}
+
+void loop()
+{
+  ULONG currentTime = millis();
+  buttonPress = checkButtonPress();
+
+  switch (currentMode)
+  {
+  case STANDARD:
+    if (buttonPress == SHORT_PRESS)
+    {
+      currentMode = SETTING_MAX_PWM;
+      lastModeChangeTime = currentTime;
+    }
+    else if (buttonPress == LONG_PRESS)
+    {
+      currentMode = SETTING_MIN_BRIGHTNESS;
+      lastModeChangeTime = currentTime;
+    }
+    break;
+
+  case SETTING_MAX_PWM:
+    if (currentTime - lastModeChangeTime > 180000)
+    { // 3 minutes timeout
+      currentMode = STANDARD;
+    }
+    else if (buttonPress == SHORT_PRESS)
+    {
+      maxPwm = getAdjustmentValuePwmMapped();
+      currentMode = STANDARD;
+    }
+    break;
+
+  case SETTING_MIN_BRIGHTNESS:
+    if (currentTime - lastModeChangeTime > 180000)
+    { // 3 minutes timeout
+      currentMode = STANDARD;
+    }
+    else if (buttonPress == SHORT_PRESS)
+    {
+      photoresistorAdcValueMin = getSensorValue();
+      currentMode = STANDARD;
+    }
+    break;
+  }
+
+  updateLedBrightness();
 }
--- Projekty/Sterownik_LED_Symulacja_swiatla_dziennego/global.h
+++ Projekty/Sterownik_LED_Symulacja_swiatla_dziennego/global.h
@@ -1,6 +1,8 @@
 
 #define INT int
 #define CHAR char
+#define BOOL char
 
 #define UINT unsigned int
 #define UCHAR unsigned char
+#define ULONG unsigned long
